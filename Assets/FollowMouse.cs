a6186164ec2ccf407596b5c7f37733d1d0702643
GJohnson613 <gabejohnson61303@gmail.com>
2024-06-21 10:34:47 -0400

    Fixed FollowMouse
    the followMouse script works and the player spaceship location has been slightly changed

--- Assets/FollowMouse.cs
+++ Assets/FollowMouse.cs
@@ -10,16 +10,11 @@ public class FollowMouse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePosition = Input.mousePosition;
-        mousePosition.z = 0;
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        Vector3 direction = Camera.main.WorldToScreenPoint(transform.position);
-        mousePosition.x = mousePosition.x - direction.x;
-        mousePosition.y = mousePosition.y - direction.y;
-
-        float angle = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
-        angle -= 90;
-        transform.rotation = Quaternion.Euler(new Vector3(90, 0, angle));
+        Vector2 direction = mousePosition - transform.position;
+        float angle = Vector2.SignedAngle(Vector2.up, direction);
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
 
     }
 }
