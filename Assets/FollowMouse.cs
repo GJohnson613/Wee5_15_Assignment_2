8d7a4e8b2eed7a187097f2e6398bf10f65613e5b
GJohnson613 <gabejohnson61303@gmail.com>
2024-06-21 09:13:48 -0400

    updated followMouse Script
    made follow mouse script functional

--- Assets/FollowMouse.cs
+++ Assets/FollowMouse.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class FollowMouse : MonoBehaviour
 {
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 0;
+
+        Vector3 direction = Camera.main.WorldToScreenPoint(transform.position);
+        mousePosition.x = mousePosition.x - direction.x;
+        mousePosition.y = mousePosition.y - direction.y;
+
+        float angle = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
+        angle -= 90;
+        transform.rotation = Quaternion.Euler(new Vector3(90, 0, angle));
+
     }
 }
