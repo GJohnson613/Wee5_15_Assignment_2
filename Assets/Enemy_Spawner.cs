7a4054f6c70b935d6952d274b910edd2bce1eb87
GJohnson613 <gabejohnson61303@gmail.com>
2024-08-08 09:30:56 -0400

    spawner changes
    changes to enemy spawner

--- Assets/Enemy_Spawner.cs
+++ Assets/Enemy_Spawner.cs
@@ -7,6 +7,7 @@ public class Enemy_Spawner : MonoBehaviour
     public GameObject enemy;
     float timeDelay = 3;
     float increment = 0.1f;
+    
 
     // Start is called before the first frame update
     void Start()
