de4df3560fde453ffedcf878dbe7f094fbff8f88
GJohnson613 <gabejohnson61303@gmail.com>
2024-08-09 11:31:19 -0400

    laser despawns
    laser despawns after a few seconds

--- Assets/LaserMovement.cs
+++ Assets/LaserMovement.cs
@@ -8,7 +8,7 @@ public class LaserMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 1f);
     }
 
     // Update is called once per frame
