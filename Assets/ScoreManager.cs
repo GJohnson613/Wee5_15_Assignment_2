04e6fef53d6ea1c250448333529ed77042788629
GJohnson613 <gabejohnson61303@gmail.com>
2024-08-12 10:07:30 -0400

    score counter works
    score counter now calculates score based on time

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -1,23 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
 
-    public Text scoreText;
+    public TextMeshProUGUI scoreText;
+    public float score = 0;
+    public int pointsPerSecond = 1;
 
-    int score = 0;
     // Start is called before the first frame update
     void Start()
     {
-        scoreText.text = score.ToString();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        scoreText.text = Mathf.FloorToInt(score).ToString();
+        score += pointsPerSecond * Time.deltaTime;
     }
 }
