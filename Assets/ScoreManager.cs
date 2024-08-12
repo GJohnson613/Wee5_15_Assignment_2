6ce90d9d45bb9333e14ecc22e49dcc8af5b536b8
GJohnson613 <gabejohnson61303@gmail.com>
2024-08-12 10:44:39 -0400

    high score counter works
    the high score counter is functional

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -7,19 +7,26 @@ public class ScoreManager : MonoBehaviour
 {
 
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI highscoreText;
     public float score = 0;
     public int pointsPerSecond = 1;
+    public float highscore = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        highscore = PlayerPrefs.GetFloat("highscore", 0);
     }
 
     // Update is called once per frame
     void Update()
     {
         scoreText.text = Mathf.FloorToInt(score).ToString();
+        highscoreText.text = "Highscore:" + Mathf.FloorToInt(highscore).ToString();
         score += pointsPerSecond * Time.deltaTime;
+        if (highscore < score)
+        {
+            PlayerPrefs.SetFloat("highscore", score);
+        }
     }
 }
