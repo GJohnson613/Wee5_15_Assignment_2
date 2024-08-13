d4925faa68dd8981ce7d3c91517a17be02a77161
GJohnson613 <gabejohnson61303@gmail.com>
2024-08-13 11:51:29 -0400

    life counter works
    the life counter works

--- Assets/LifeCounter.cs
+++ Assets/LifeCounter.cs
@@ -7,16 +7,47 @@ public class LifeCounter : MonoBehaviour
     public GameObject Life1;
     public GameObject Life2;
     public GameObject Life3;
+    int lifeNumber;
+    public GameObject enemy;
+    public GameObject collisionSquare;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Life1.gameObject.SetActive(true);
+        Life2.gameObject.SetActive(true);
+        Life3.gameObject.SetActive(true);
+        lifeNumber = 3;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("its here");
+        if (collision.gameObject.tag == "Enemy")
+        {
+            updateLives();
+        }
+    }
+
+    void updateLives()
+    {
+        lifeNumber -= 1;
+        if(lifeNumber == 2)
+        {
+            Life3.gameObject.SetActive(false);
+        }
+        if(lifeNumber  == 1)
+        {
+            Life2.gameObject.SetActive(false);
+        }
+        if(lifeNumber == 0)
+        {
+            Life1.gameObject.SetActive(false);
+        }
     }
 }
