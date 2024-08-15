c7df5771200d349be23f7a981292a5d8e4ace82a
GJohnson613 <gabejohnson61303@gmail.com>
2024-08-15 10:09:58 -0400

    menu buttons work?
    menu buttons work?

---
+++ Assets/MainMenuFunctions.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuFunctions : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject Credits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenuButton();
+    }
+
+    public void PlayButton()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Game Level");
+    }
+
+    public void CreditsButton()
+    {
+        MainMenu.SetActive(false);
+        Credits.SetActive(true);
+    }
+
+    public void MainMenuButton()
+    {
+        MainMenu.SetActive(true);
+        Credits.SetActive(false);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
