16269f4211eb3fb4452e0bac4cbc3a3cf3a05e3f
GJohnson613 <gabejohnson61303@gmail.com>
2024-08-16 11:02:24 -0400

    audio manager set up
    audio manager has room for all needed sounds

--- Assets/audioManager.cs
+++ Assets/audioManager.cs
@@ -6,6 +6,23 @@ public class audioManager : MonoBehaviour
 {
     [SerializeField] private AudioSource laserSound;
     [SerializeField] private AudioSource explosionSound;
+
+    [SerializeField] private AudioSource lifeLostSound;
+
+    [SerializeField] private AudioSource titleMusic;
+    [SerializeField] private AudioSource gameMusic;
+
+    public void PlayTitleMusic()
+    {
+        titleMusic.Play();
+        gameMusic.Stop();
+    }
+
+    public void PlayGameMusic()
+    {
+        gameMusic.Play();
+        titleMusic.Stop();
+    }
     // Start is called before the first frame update
     void Start()
     {
